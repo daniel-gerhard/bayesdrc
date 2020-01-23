812e813d49fa5a43d1acbf416b1a51d6b482549a
Daniel Gerhard <00gerhard@gmail.com>
2020-01-23 14:47:24 +1300

    basic MH algorithm in bdrm.fit()

--- R/bdrm.r
+++ R/bdrm.r
@@ -1,8 +1,8 @@
-bdrm <- function(formula, data, family){
+bdrm <- function(formula, data, model, iter){
   UseMethod("bdrm")
 }
 
-bdrm.formula <- function(formula, data, family, model){
+bdrm.formula <- function(formula, data, model, iter){
   cl <- match.call()
   mf <- match.call(expand.dots = FALSE)
   m <- match(c("formula", "data", "subset", "weights", "na.action", "offset"), names(mf), 0L)
@@ -19,7 +19,7 @@ bdrm.formula <- function(formula, data, family, model){
 
 test <- data.frame(response = c(1, 2, 3, 4), 
                    dose = c(4, 3, 2, 1))
-bdrm(response ~ dose, data=test, family="gaussian")
+bdrm(response ~ dose, data=test)
 
 
 logistic <- function(fixed=c(NA, NA, NA, NA, NA), 
@@ -29,6 +29,44 @@ logistic <- function(fixed=c(NA, NA, NA, NA, NA),
                      upr=c(Inf, Inf, Inf, Inf, Inf)){
 
   fct <- function(x, beta) beta[2] + (beta[3]-beta[2])/(1 + exp(-beta[1]*(x-beta[4]))^beta[5])  
-  mod <- list(fct, prior.mu, prior.sd, lwr, upr)
+  loglik <- function(x, y, beta, variance){
+    mu <- fct(x, beta)
+    LL <- sum(dnorm(y, mu, 1/sqrt(variance), log=TRUE))
+    return(LL)
+  }
+  ss <- function(x, y, beta){
+    mu <- richards(x, beta)
+    SS <- sum((y-mu)^2)
+    return(SS)
+  }
+  mod <- list(fct, prior.mu, prior.sd, lwr, upr, loglik, ss)
   return(mod)
+}
+
+
+bdrm.fit <- function(x, y, model, iter){
+  p <- length(model$fixed)
+  bmu <- model$prior.mu
+  bsd <- model$prior.sd
+  clwr <- model$lwr
+  cupr <- model$upr
+  loglik <- model$loglik
+  for (i in 1:iter){
+    for (j in 1:p){
+      bn[j] <- rtruncnorm(1, a=clwr[j], b=cupr[j], mean=bo[j], sd=0.5)
+      logR <- loglik(x, y, bn) - 
+        loglik(x, y, bo) +
+        log(dtruncnorm(bn[j], a=clwr[j], b=cupr[j], mean=bmu[j], sd=bsd[j])) - 
+        log(dtruncnorm(bo[j], a=clwr[j], b=cupr[j], mean=bmu[j], sd=bsd[j])) -
+        log(dtruncnorm(bn[j], a=clwr[j], b=cupr[j], mean=bo[j], sd=0.5)) +
+        log(dtruncnorm(bo[j], a=clwr[j], b=cupr[j], mean=bn[j], sd=0.5))
+      logU <- log(runif(1, 0, 1))
+      if (logR > logU){
+        bo <- bn
+        #accept[1] <- accept[1] + 1
+      } else {
+        bn <- bo
+      }
+    }
+  }
 }
