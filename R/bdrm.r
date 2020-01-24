b2153293e13227cf10833d50c805de5336b2af8a
Daniel Gerhard <00gerhard@gmail.com>
2020-01-24 14:11:03 +1300

    MH multiple chains

--- R/bdrm.r
+++ R/bdrm.r
@@ -17,14 +17,9 @@ bdrm.formula <- function(formula, data, model, iter){
 }
 
 
-test <- data.frame(response = c(1, 2, 3, 4), 
-                   dose = c(4, 3, 2, 1))
-bdrm(response ~ dose, data=test)
-
-
 logistic <- function(fixed=c(NA, NA, NA, NA, NA), 
-                     prior.mu=c(),
-                     prior.sd=c(),
+                     prior.mu=c(10, 0, 1, 0.5, 1),
+                     prior.sd=c(1, 1, 1, 1, 1),
                      lwr=c(-Inf, -Inf, -Inf, -Inf, -Inf),
                      upr=c(Inf, Inf, Inf, Inf, Inf)){
 
@@ -35,38 +30,69 @@ logistic <- function(fixed=c(NA, NA, NA, NA, NA),
     return(LL)
   }
   ss <- function(x, y, beta){
-    mu <- richards(x, beta)
+    mu <- fct(x, beta)
     SS <- sum((y-mu)^2)
     return(SS)
   }
-  mod <- list(fct, prior.mu, prior.sd, lwr, upr, loglik, ss)
+  mod <- list(fct=fct, fixed=fixed, prior.mu=prior.mu, prior.sd=prior.sd, lwr=lwr, upr=upr, loglik=loglik, ss=ss)
   return(mod)
 }
 
+library(truncnorm)
+dose = seq(0, 1, length=25)
+model <- logistic()
+test <- data.frame(response = rnorm(length(dose), model$fct(dose, model$prior.mu), 0.1), 
+                   dose=dose)
+plot(response ~ dose, data=test)
+bdrm(response ~ dose, data=test)
+iter <- 100
+startval <- c(10, 0, 1, 0.5, 1)
+x <- test$dose
+y <- test$response
 
-bdrm.fit <- function(x, y, model, iter){
+chains <- 4
+
+bdrm.fit <- function(x, y, model, chains, iter, startval){
   p <- length(model$fixed)
   bmu <- model$prior.mu
   bsd <- model$prior.sd
   clwr <- model$lwr
   cupr <- model$upr
   loglik <- model$loglik
-  for (i in 1:iter){
-    for (j in 1:p){
-      bn[j] <- rtruncnorm(1, a=clwr[j], b=cupr[j], mean=bo[j], sd=0.5)
-      logR <- loglik(x, y, bn) - 
-        loglik(x, y, bo) +
-        log(dtruncnorm(bn[j], a=clwr[j], b=cupr[j], mean=bmu[j], sd=bsd[j])) - 
-        log(dtruncnorm(bo[j], a=clwr[j], b=cupr[j], mean=bmu[j], sd=bsd[j])) -
-        log(dtruncnorm(bn[j], a=clwr[j], b=cupr[j], mean=bo[j], sd=0.5)) +
-        log(dtruncnorm(bo[j], a=clwr[j], b=cupr[j], mean=bn[j], sd=0.5))
-      logU <- log(runif(1, 0, 1))
-      if (logR > logU){
-        bo <- bn
-        #accept[1] <- accept[1] + 1
-      } else {
-        bn <- bo
+  ss <- model$ss
+  jsd <- sqrt(bsd^2 * (2.4/sqrt(p))^2)
+  # adaptive phase
+  apm <- array(NA, dim=c(iter, length(startval), chains))
+  avar <- matrix(NA, nrow=iter, ncol=chains)
+  apm[1,,] <- startval
+  avar[1,] <- 1
+  atau <- 0.001
+  btau <- 0.001
+  
+  for (k in 1:chains){
+    for (i in 2:iter){
+      bn <- bo <- apm[i-1,,k]
+      av <- avar[i-1,k]
+      for (j in 1:p){
+        bn[j] <- rtruncnorm(1, a=clwr[j], b=cupr[j], mean=bo[j], sd=jsd[j])
+        logR <- loglik(x, y, bn, av) - 
+          loglik(x, y, bo, av) +
+          log(dtruncnorm(bn[j], a=clwr[j], b=cupr[j], mean=bmu[j], sd=bsd[j])) - 
+          log(dtruncnorm(bo[j], a=clwr[j], b=cupr[j], mean=bmu[j], sd=bsd[j])) -
+          log(dtruncnorm(bn[j], a=clwr[j], b=cupr[j], mean=bo[j], sd=jsd[j])) +
+          log(dtruncnorm(bo[j], a=clwr[j], b=cupr[j], mean=bn[j], sd=jsd[j]))
+        logU <- log(runif(1, 0, 1))
+        if (logR > logU){
+          bo <- bn
+          #accept[1] <- accept[1] + 1
+        } else {
+          bn <- bo
+        }
       }
+      avar[i,k] <- rgamma(1, atau + length(y)/2, btau + 0.5 * ss(x, y, bo))
+      apm[i,,k] <- bn
     }
   }
+  #### burnin
+  #### sampling
 }
