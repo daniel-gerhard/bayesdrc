7e56c97218294bcd44a0b9eccf3221a4e1d99ee6
Daniel Gerhard <00gerhard@gmail.com>
2020-01-27 11:14:22 +1300

    added MH sampling stage

--- R/bdrm.r
+++ R/bdrm.r
@@ -45,12 +45,15 @@ test <- data.frame(response = rnorm(length(dose), model$fct(dose, model$prior.mu
                    dose=dose)
 plot(response ~ dose, data=test)
 bdrm(response ~ dose, data=test)
-iter <- 1000
+aiter <- 1000
+iter <- 10000
 startval <- c(10, 0, 1, 0.5, 1)
 x <- test$dose
 y <- test$response
 
 chains <- 4
+
+pm <- bdrm.fit(x, y, model, chains, iter, startval)
 
 bdrm.fit <- function(x, y, model, chains, iter, startval){
   p <- length(model$fixed)
@@ -62,8 +65,8 @@ bdrm.fit <- function(x, y, model, chains, iter, startval){
   ss <- model$ss
   jsd <- sqrt(bsd^2 * (2.4/sqrt(p))^2)
   # adaptive phase
-  apm <- array(NA, dim=c(iter, length(startval), chains))
-  avar <- matrix(NA, nrow=iter, ncol=chains)
+  apm <- array(NA, dim=c(aiter, length(startval), chains))
+  avar <- matrix(NA, nrow=aiter, ncol=chains)
   apm[1,,] <- startval
   avar[1,] <- 1
   atau <- 0.001
@@ -72,7 +75,7 @@ bdrm.fit <- function(x, y, model, chains, iter, startval){
   accept <- matrix(0, nrow=chains, ncol=p)
   
   for (k in 1:chains){
-    for (i in 2:iter){
+    for (i in 2:aiter){
       bn <- bo <- apm[i-1,,k]
       av <- avar[i-1,k]
       for (j in 1:p){
@@ -90,10 +93,10 @@ bdrm.fit <- function(x, y, model, chains, iter, startval){
         } else {
           bn <- bo
         }
-        if (i %in% seq(100, iter, by=100)){ 
+        if (i == 200) jsd[j] <- sqrt(var(apm[50:199,j,k]) * (2.4/sqrt(p))^2)
+        if (i %in% seq(300, aiter, by=100)){ 
           if (accept[k,j]/100 < 0.4) jsd[j] <- jsd[j]*0.95
           if (accept[k,j]/100 > 0.55) jsd[j] <- jsd[j]*1.05
-          print(accept[k,])
           accept[k,j] <- 0
         }
       }
@@ -101,9 +104,43 @@ bdrm.fit <- function(x, y, model, chains, iter, startval){
       apm[i,,k] <- bn
     }
   }
-  accept/iter
-  plot(apm[,1,1], type="l")
+  #accept/aiter
+  #plot(apm[,5,1], type="l")
   
   #### burnin
+  pm <- array(NA, dim=c(iter, length(startval), chains))
+  var <- matrix(NA, nrow=iter, ncol=chains)
+  pm[1,,] <- apm[aiter,,]
+  var[1,] <- avar[aiter,]
+  accept <- matrix(0, nrow=chains, ncol=p)
+  
+  for (k in 1:chains){
+    for (i in 2:iter){
+      bn <- bo <- pm[i-1,,k]
+      av <- var[i-1,k]
+      for (j in 1:p){
+        bn[j] <- rtruncnorm(1, a=clwr[j], b=cupr[j], mean=bo[j], sd=jsd[j])
+        logR <- loglik(x, y, bn, av) - 
+          loglik(x, y, bo, av) +
+          log(dtruncnorm(bn[j], a=clwr[j], b=cupr[j], mean=bmu[j], sd=bsd[j])) - 
+          log(dtruncnorm(bo[j], a=clwr[j], b=cupr[j], mean=bmu[j], sd=bsd[j])) -
+          log(dtruncnorm(bn[j], a=clwr[j], b=cupr[j], mean=bo[j], sd=jsd[j])) +
+          log(dtruncnorm(bo[j], a=clwr[j], b=cupr[j], mean=bn[j], sd=jsd[j]))
+        logU <- log(runif(1, 0, 1))
+        if (logR > logU){
+          bo <- bn
+          accept[k,j] <- accept[k,j] + 1
+        } else {
+          bn <- bo
+        }
+      }
+      var[i,k] <- rgamma(1, atau + length(y)/2, btau + 0.5 * ss(x, y, bo))
+      pm[i,,k] <- bn
+    }
+  }
+  #accept/iter
+  #plot(pm[,1,1], type="l")
+  
   #### sampling
+  return(pm)
 }
