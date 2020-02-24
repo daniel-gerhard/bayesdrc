5cab140ad313c73389aa398d6878c9619ea3e331
Daniel Gerhard <00gerhard@gmail.com>
2020-02-25 10:36:24 +1300

    add weibull2 parameterisation

--- R/modelfunctions.r
+++ R/modelfunctions.r
@@ -58,12 +58,35 @@ logistic <- function(names=c("b1", "b2", "b3", "b4", "b5")){
 #' @details 
 #' The logistic function is defined as 
 #' \deqn{f(x, \beta) = \beta_2 + \beta_3 \exp(- \exp( -\beta_1 (\log(x)-\log(\beta_4)))) }
+#' there exists a second parameterisation that is implemented in function \code{weibull2}
+#' \deqn{f(x, \beta) = \beta_2 + \beta_3 (1 - \exp(- \exp( -\beta_1 (\log(x)-\log(\beta_4))))) }
 #' 
 #' @keywords models
 
 weibull1 <- function(names=c("b1", "b2", "b3", "b4")){
   fct <- function(x, beta){
     beta[2] + beta[3]*exp(-exp(-beta[1]*(log(x)-log(beta[4]))))  
+  }
+  loglik <- function(y, mu, variance){
+    LL <- sum(dnorm(y, mu, 1/sqrt(variance), log=TRUE))
+    return(LL)
+  }
+  ss <- function(y, mu){
+    SS <- sum((y-mu)^2)
+    return(SS)
+  }
+  mod <- list(fct=fct, 
+              loglik=loglik, 
+              ss=ss, 
+              p=4,
+              names=names)
+  return(mod)
+}
+
+#' @rdname weibull1
+weibull2 <- function(names=c("b1", "b2", "b3", "b4")){
+  fct <- function(x, beta){
+    beta[2] + beta[3]*(1 - exp(-exp(-beta[1]*(log(x)-log(beta[4])))))  
   }
   loglik <- function(y, mu, variance){
     LL <- sum(dnorm(y, mu, 1/sqrt(variance), log=TRUE))
