4e79f72d4856c6bc95f491683975e66f5fa845e5
Daniel Gerhard <00gerhard@gmail.com>
2020-02-24 15:16:12 +1300

    adding weibull1 function

--- R/modelfunctions.r
+++ R/modelfunctions.r
@@ -40,3 +40,43 @@ logistic <- function(names=c("b1", "b2", "b3", "b4", "b5")){
 }
 
 
+#' Weibull Dose-Response Model
+#' 
+#' Assuming a Weibull dose-response relationship in a bdrm model.
+#' 
+#' @param names a vector with names for each model parameter
+#' 
+#' @return a list containing at least the following components:
+#' \describe{
+#'   \item{fct}{the logistic function with a dose x and a vector of parameters beta}
+#'   \item{loglik}{the log-likelihood function}
+#'   \item{ss}{sum-of-squares for observed and fitted values}
+#'   \item{p}{number of model parameters}
+#'   \item{names}{a vector with parameter names}
+#' }
+#' 
+#' @details 
+#' The logistic function is defined as 
+#' \deqn{f(x, \beta) = \beta_2 + \beta_3 \exp(- \exp( \beta_1 (\log(x)-\log(\beta_4)))) }
+#' 
+#' @keywords models
+
+weibull1 <- function(names=c("b1", "b2", "b3", "b4")){
+  fct <- function(x, beta){
+    beta[2] + beta[3]*exp(-exp(beta[1]*(log(x)-log(beta[4]))))  
+  }
+  loglik <- function(y, mu, variance){
+    LL <- sum(dnorm(y, mu, 1/sqrt(variance), log=TRUE))
+    return(LL)
+  }
+  ss <- function(y, mu){
+    SS <- sum((y-mu)^2)
+    return(SS)
+  }
+  mod <- list(fct=fct, 
+              loglik=loglik, 
+              ss=ss, 
+              p=4,
+              names=names)
+  return(mod)
+}
